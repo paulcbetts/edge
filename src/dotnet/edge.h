10936ec09ba0b0c68da6096bc2ceeacacf5895f0
Paul Betts <paul@paulbetts.org>
2015-11-13 14:59:22 -0800

    Fix up the headers to match

--- src/dotnet/edge.h
+++ src/dotnet/edge.h
@@ -1,17 +1,17 @@
 /**
- * Portions Copyright (c) Microsoft Corporation. All rights reserved. 
- * 
+ * Portions Copyright (c) Microsoft Corporation. All rights reserved.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  http://www.apache.org/licenses/LICENSE-2.0  
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
- * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR 
- * PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+ * PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
  *
- * See the Apache Version 2.0 License for specific language governing 
+ * See the Apache Version 2.0 License for specific language governing
  * permissions and limitations under the License.
  */
 #ifndef __EDGE_H
@@ -113,7 +113,7 @@ public:
 ref class ClrFuncReflectionWrap {
 private:
     System::Object^ instance;
-    MethodInfo^ invokeMethod;    
+    MethodInfo^ invokeMethod;
 
     ClrFuncReflectionWrap();
 
@@ -133,11 +133,11 @@ private:
 
 public:
     static NAN_METHOD(Initialize);
-    static v8::Local<v8::Function> Initialize(System::Func<System::Object^,Task<System::Object^>^>^ func);
+    static v8::Local<v8::Function> Initialize(System::Func<System::Object^,Task<System::Object^>^>^ func, System::String^ assemblyLocation);
     v8::Local<v8::Value> Call(v8::Local<v8::Value> payload, v8::Local<v8::Value> callback);
     static v8::Local<v8::Value> MarshalCLRToV8(System::Object^ netdata);
     static v8::Local<v8::Value> MarshalCLRExceptionToV8(System::Exception^ exception);
-    static System::Object^ MarshalV8ToCLR(v8::Local<v8::Value> jsdata);    
+    static System::Object^ MarshalV8ToCLR(v8::Local<v8::Value> jsdata);
 };
 
 typedef struct clrFuncWrap {
